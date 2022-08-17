b216f1e94c3b7786102061628c36165ae12eb8ed
Quentin Orhant <quentin.orhant@gmail.com>
2022-08-17 18:32:33 +0200

    Redefine pins and clean the code

--- Interrupt_driven_4-1stepper_encoder_accel_rotary_interrupt/Interrupt_driven_4-1stepper_encoder_accel_rotary_interrupt.ino
+++ Interrupt_driven_4-1stepper_encoder_accel_rotary_interrupt/Interrupt_driven_4-1stepper_encoder_accel_rotary_interrupt.ino
@@ -1,30 +1,48 @@
-// Basic sketch for A4988 stepper motor driver
-// This sketch uses interrupts to step the motor
-
-// MIT licence applies
-// Created by Bodmer 20/4/2016
-
-// Microsteps per step   :     1      8     16   (Set with A4988 mode pins)
-// RPM at 5kHz interrupt :   1500    187    93   (for a motor with 200 steps per rev)
-// Approx. minimum RPM   :     74     10     5   (for a motor with 200 steps per rev)
-
-// Use timer prescaler to allow lower speeds, e.g. divide clock by 8
-// Use fewer microsteps per step to raise speeds
-
-// gestion moteur3 en position
 #include <AccelStepper.h>
-
-// gestion ecran oled ssd1306
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 #include <RotaryEncoder.h>
-
 #include "display.h"
 
+//////////////////////DEFINE PINS//////////////////////
+
+// Optical sensors for slider position
+#define SLIDER_MAX_PIN  3
+#define SLIDER_MIN_PIN  14
+
+// Micro switchs for winder speed
+#define WINDER_MAX_PIN  57  
+#define WINDER_MIN_PIN  1
+
+// Motor PULLER : X sur ramps 1.4
+#define MOTOR1_STEP_ENA 38               // enable pin
+#define MOTOR1_STEP_CLK 54               // clock pin
+#define MOTOR1_STEP_DIR 55               // direction pin
+
+// Motor SLIDER : Y sur ramps 1.4
+#define MOTOR3_STEP_ENA 56               // enable pin
+#define MOTOR3_STEP_CLK 60               // clock pin
+#define MOTOR3_STEP_DIR 61               // direction pin
+
+// Motor WINDER : Z sur ramps 1.4
+#define MOTOR2_STEP_ENA 62               // enable pin
+#define MOTOR2_STEP_CLK 46               // clock pin
+#define MOTOR2_STEP_DIR 48               // direction pin
+
+// Fan ramp
+#define FAN_PIN 8
+
+// Rotary encoder
+const int clkPin  = 32;
+const int dtPin  = 47;
+const int swPin  = 45;
+
+///////////////////////////////////////////////////////
+
+// Oled screen settings
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 64
-
 #define OLED_RESET -1
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -42,43 +60,11 @@ Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 
 #define TIMER_2_MIN         1000 // Stop speed for end of movement ramp down
 #define TIMER_2_MAX         0xFFFF - ( CPU_MHZ / PRESCALER / (STEPS_PER_REV * MICROSTEPS_PER_STEP * MAX_RPM / 60L) )
 
-// motor PULLER X sur ramps 1.4
-#define MOTOR1_STEP_ENA 38               // A4988 enable pin
-#define MOTOR1_STEP_CLK 54               // A4988 clock pin
-#define MOTOR1_STEP_DIR 55               // A4988 direction pin
-
-
-// motor WINDER Y sur ramps 1.4
-//#define MOTOR2_STEP_ENA 56               // A4988 enable pin
-//#define MOTOR2_STEP_CLK 60               // A4988 clock pin
-//#define MOTOR2_STEP_DIR 61               // A4988 direction pin
-#define MOTOR2_STEP_ENA 62               // A4988 enable pin
-#define MOTOR2_STEP_CLK 46               // A4988 clock pin
-#define MOTOR2_STEP_DIR 48               // A4988 direction pin
-#define MOTOR2_STEP_PER_TURN    (200 * 2 * 32)
-// motor SLIDER Z sur ramps 1.4
-//#define MOTOR3_STEP_ENA 62               // A4988 enable pin
-//#define MOTOR3_STEP_CLK 46               // A4988 clock pin
-//#define MOTOR3_STEP_DIR 48               // A4988 direction pin
-#define MOTOR3_STEP_ENA 56               // A4988 enable pin
-#define MOTOR3_STEP_CLK 60               // A4988 clock pin
-#define MOTOR3_STEP_DIR 61               // A4988 direction pin
-
-
-#define SLIDER_MAX_PIN  3
-#define SLIDER_MIN_PIN  14
-
-#define WINDER_MAX_PIN  57  
-#define WINDER_MIN_PIN  18
-
-#define POTENTIMETER_SPEED_PIN    A11       //         
-// parametres pour moteurs sous interupt
-
+// Parameters for motors with interrupt
 unsigned int timer1_counter = 1;
 unsigned int timer2_counter = 1;
 volatile unsigned int new_counter = 1;
 volatile unsigned int new_counter2 = 1;
-
 volatile int  steps = 0;    // Changes outside of interrupt handler, so make it volatile
 volatile int  steps2 = 0;    // Changes outside of interrupt handler, so make it volatile
 volatile bool done  = true; // Used outside interrupt handler, so make it volatile
@@ -86,63 +72,51 @@ volatile bool done2  = true; // Used outside interrupt handler, so make it volat
 volatile uint32_t motor2_step = 0;
 int8_t motor3_dir = 1;   // direction du moteur3
 long motor3_position = 0;
-
-#define WINDER_MAX_SPEED 40*8
-#define WINDER_MIN_SPEED 1
-
 uint32_t winder_speed = WINDER_MIN_SPEED;
-
-#define MAX_MOTOR3_POSITION 4000
-#define MOTOR3_STEP ((200 * 32) /8)
-//(200 * 32)  //1tour
-
+bool step_forward = true;
 int motorSpeed = 16;     //max puller speed
 
-bool step_forward = true;
+#define WINDER_MAX_SPEED 20
+#define WINDER_MIN_SPEED 1
+#define MAX_MOTOR3_POSITION 4000
+#define MOTOR2_STEP_PER_TURN    (200 * 2 * 32)
+#define MOTOR3_STEP ((200 * 32) /8)  //(200 * 32)  //1tour
 
-// parametres pour moteur asservi en position
+// Parameter pour moteur asservi en position
 AccelStepper stepper;
 
 // Encoder parameters
-const int clkPin  = 32;
-const int dtPin  = 47;
-const int swPin  = 45;
-
 const int minrotary = 1;
 const int maxrotary = 32;
 
-//Variables
+// Encoder variables
 int rotVal  = 0;
 bool clkState  = LOW;
 bool clkLast  = HIGH;
 bool swState  = HIGH;
 bool swLast  = HIGH;
 
-
 RotaryEncoder *encoder = nullptr;
 
-void checkPosition()
-{
-  if (encoder != nullptr)
-    encoder->tick(); // just call tick() to check the state.
+void checkPosition(){
+    if (encoder != nullptr)
+      encoder->tick(); // just call tick() to check the state.
 }
-
-void initRotary ()
-{
+  
+void initRotary (){
   pinMode(clkPin,INPUT);
   pinMode(dtPin,INPUT);
   pinMode(swPin,INPUT_PULLUP);
   encoder = new RotaryEncoder(clkPin, dtPin, RotaryEncoder::LatchMode::FOUR0);
-  
   attachInterrupt(digitalPinToInterrupt(clkPin), checkPosition, CHANGE);
   attachInterrupt(digitalPinToInterrupt(dtPin), checkPosition, CHANGE);
 }
-
+  
 int readRotary() { /* function readRotary */
- ////Test routine for Rotary
- // gestion position
- clkState = digitalRead(clkPin);
- if ((clkLast == LOW) && (clkState == HIGH)) {//rotary moving
+  ////Test routine for Rotary
+  // gestion position
+  clkState = digitalRead(clkPin);
+  if ((clkLast == LOW) && (clkState == HIGH)) {//rotary moving
    //Serial.print("Rotary position ");
    if (digitalRead(dtPin) == HIGH) {
      rotVal = rotVal - 1;
@@ -158,22 +132,23 @@ int readRotary() { /* function readRotary */
    }
    //Serial.println(rotVal);
    delay(200);
- }
- clkLast = clkState;
- //gestion bouton
- swState = digitalRead(swPin);
- if (swState == LOW && swLast == HIGH) {
+  }
+  clkLast = clkState;
+  //gestion bouton
+  swState = digitalRead(swPin);
+  if (swState == LOW && swLast == HIGH) {
    Serial.println("Rotary pressed");
    delay(100);//debounce
- }
- swLast = swState;
- return (rotVal);
+  }
+  swLast = swState;
+  return (rotVal);
 }
+
 // fin encoder
 
 uint32_t timewinder = 0;
-void HandleWinderSpeed ()
-{
+
+void HandleWinderSpeed (){
   if(millis ()- timewinder > 10)
   {
     if(digitalRead (WINDER_MAX_PIN) ==HIGH)
@@ -193,85 +168,76 @@ void HandleWinderSpeed ()
 }
 
 // gere la position du moteur 3 en fonction du moteur2
-void HandleMotor3 ()
-{
-#if 0
+void HandleMotor3 (){
+  #if 0
     if (digitalRead(SLIDER_MAX_PIN) == HIGH || digitalRead(SLIDER_MIN_PIN)==HIGH)
     {
         motor3_position = stepper.currentPosition ();
         stepper.moveTo(motor3_position); 
     }
-#endif    
-    if(digitalRead (SLIDER_MAX_PIN) == HIGH && motor3_dir > 0)
-    {
-        Serial.println ("slider motor on max");
-        motor3_dir = -1;
-        motor3_position = stepper.currentPosition() - MOTOR3_STEP;
-        stepper.moveTo (motor3_position);
-        Serial.print ("slider motor on position ");
-        Serial.println (stepper.currentPosition());
+  #endif    
+  if(digitalRead (SLIDER_MAX_PIN) == HIGH && motor3_dir > 0)
+  {
+      Serial.println ("slider motor on max");
+      motor3_dir = -1;
+      motor3_position = stepper.currentPosition() - MOTOR3_STEP;
+      stepper.moveTo (motor3_position);
+      Serial.print ("slider motor on position ");
+      Serial.println (stepper.currentPosition());
+  
+  }
+  
+  if(digitalRead (SLIDER_MIN_PIN) == HIGH && motor3_dir < 0)
+  {
+      Serial.println ("slider motor on min");
+      motor3_dir = 1;
+      motor3_position = stepper.currentPosition() + MOTOR3_STEP;
+      stepper.moveTo (motor3_position);
+      Serial.print ("slider motor on position ");
+      Serial.println (stepper.currentPosition());
+  
+  }  
+  
+  //Serial.print ("slider motor on position ");
+  //Serial.println (stepper.currentPosition());
+  
+  if (motor2_step > MOTOR2_STEP_PER_TURN) 
+  {
+    //inverse le le sens du moteur si on touche un fin de course
     
+    
+    if (motor3_dir > 0)
+    {
+      motor3_position += MOTOR3_STEP;
     }
-    
-    if(digitalRead (SLIDER_MIN_PIN) == HIGH && motor3_dir < 0)
+    else
     {
-        Serial.println ("slider motor on min");
-        motor3_dir = 1;
-        motor3_position = stepper.currentPosition() + MOTOR3_STEP;
-        stepper.moveTo (motor3_position);
-        Serial.print ("slider motor on position ");
-        Serial.println (stepper.currentPosition());
-    
-    }  
-    
-    //Serial.print ("slider motor on position ");
-    //Serial.println (stepper.currentPosition());
-    
-    if (motor2_step > MOTOR2_STEP_PER_TURN) 
-    {
-      //inverse le le sens du moteur si on touche un fin de course
+      motor3_position -= MOTOR3_STEP;
       
-      
-      if (motor3_dir > 0)
-      {
-        motor3_position += MOTOR3_STEP;
-      }
-      else
-      {
-        motor3_position -= MOTOR3_STEP;
-        
-      }
-       
-       stepper.moveTo (motor3_position);
-       Serial.print ("slider motor go to position ");
-       Serial.print (motor3_dir);
-       Serial.print (" ");
-       Serial.println (motor3_position);
-    
-       noInterrupts();
-       motor2_step -= MOTOR2_STEP_PER_TURN;
-       interrupts ();
-   }
-    
+    }
+     
+     stepper.moveTo (motor3_position);
+     Serial.print ("slider motor go to position ");
+     Serial.print (motor3_dir);
+     Serial.print (" ");
+     Serial.println (motor3_position);
+  
+     noInterrupts();
+     motor2_step -= MOTOR2_STEP_PER_TURN;
+     interrupts ();
+ }
 }
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
 
-// initialisation ecran
+  // init screen
   display.begin(0X3C, true);   // initialisation de l'objet afficheur
   display.clearDisplay();
   display.display ();
-  //afficheur.setTextSize(2);                                              // choix de la taille des caractères
-  //afficheur.setTextColor(1);                                            // caractères en blanc
-  //afficheur.setCursor(32,16);                                         // positionnement du curseur
-  //afficheur.print("ARDUINO");                                         // affichage de ARDUINO
-  //afficheur.drawBitmap(0, 0,  love, 64, 32, 1);             // affichage de l'image love
   display.drawPixel(10, 10, SH110X_WHITE);
   for (int x = 8; x < 120; x++)
     display.drawPixel(x, 20, SH110X_WHITE);
-  
   display.fillRect (24, 24, 64, 64, SH110X_WHITE);
   display.setCursor (24, 2);
   display.setTextColor (SH110X_WHITE);
@@ -281,57 +247,48 @@ void setup()
 
   // init GPIO
   pinMode(LED_BUILTIN, OUTPUT);
-
   pinMode(MOTOR1_STEP_ENA, OUTPUT);
   pinMode(MOTOR1_STEP_CLK, OUTPUT);
   pinMode(MOTOR1_STEP_DIR, OUTPUT);
   pinMode(MOTOR2_STEP_ENA, OUTPUT);
   pinMode(MOTOR2_STEP_CLK, OUTPUT);
   pinMode(MOTOR2_STEP_DIR, OUTPUT);
-
-  pinMode (SLIDER_MAX_PIN,INPUT);
-  pinMode (SLIDER_MIN_PIN,INPUT);
-
+  pinMode(MOTOR3_STEP_ENA, OUTPUT);
+  pinMode(MOTOR3_STEP_CLK, OUTPUT);
+  pinMode(MOTOR3_STEP_DIR, OUTPUT);
+  pinMode(SLIDER_MAX_PIN,INPUT);
+  pinMode(SLIDER_MIN_PIN,INPUT);
   pinMode(WINDER_MAX_PIN,INPUT);
   pinMode(WINDER_MIN_PIN,INPUT);
-  
-  digitalWrite(MOTOR1_STEP_ENA, HIGH); // Disable the driver
-  digitalWrite(MOTOR2_STEP_ENA, HIGH); // Disable the driver
+  pinMode(FAN_PIN, OUTPUT);
 
+  // Turn on the fan ramp
+  digitalWrite(FAN_PIN, HIGH);
+
+  // Init motors with interrupt
   startTimer1();                // Start the timer 1 interrupt moto E0
   startTimer2();                // start the timer 2 interrupt motor E1
   digitalWrite(MOTOR1_STEP_ENA, LOW);  // Enable the driver
   digitalWrite(MOTOR2_STEP_ENA, LOW);  // Enable the driver
 
-  
   // initialise le codeur rotatif
   initRotary ();
 
   // initialise le moteur position
-  pinMode(MOTOR3_STEP_ENA, OUTPUT);
-  pinMode(MOTOR3_STEP_CLK, OUTPUT);
-  pinMode(MOTOR3_STEP_DIR, OUTPUT);
-
   stepper = AccelStepper (AccelStepper::DRIVER, MOTOR3_STEP_CLK, MOTOR3_STEP_DIR, 0, 0, true);
   stepper.setEnablePin (MOTOR3_STEP_ENA);
-  stepper.setPinsInverted  ( false, // dir
-        false, // step
-        true    // enable
-        );   
-
+  stepper.setPinsInverted(false, false, true ); // (dir,step,enable)
   stepper.enableOutputs ();
   stepper.setMaxSpeed(20000.0);
   stepper.setAcceleration(8000.0);
 }
 
 uint32_t time_display = 0;
-void loop()
-{
+
+void loop(){
   digitalWrite(LED_BUILTIN, HIGH);
   encoder->tick();
 
-  
-  int pot = analogRead(POTENTIMETER_SPEED_PIN);
   int valencoder = encoder->getPosition ();
 
   if (valencoder < 0)
@@ -341,15 +298,14 @@ void loop()
     
   int spd = map(valencoder, minrotary, maxrotary, 1, motorSpeed);
   
-  
-  
   #if 1
-  Serial.print (motor2_step);
-  Serial.print (" ");
-  Serial.print (valencoder);
-  Serial.print (" ");
-  Serial.println (spd);
-#endif
+    Serial.print (motor2_step);
+    Serial.print (" ");
+    Serial.print (valencoder);
+    Serial.print (" ");
+    Serial.println (spd);
+  #endif
+  
   // vitesse des 2 premiers moteurs
   motorRPM(spd);  // moteur1 en fonction du pontentiometre
   motor2RPM(winder_speed);  // moteur2 a vitesse fixe
@@ -358,7 +314,6 @@ void loop()
   stepMotor(+6400 * 2); // Positive for forward movement  400 steps * 16 microsteps
   stepMotor2(-6400 * 2); // Positive for forward movement  400 steps * 16 microsteps
       
-  // Blink LED
   HandleMotor3();
 
   //get winder speed
@@ -369,14 +324,11 @@ void loop()
   digitalWrite(LED_BUILTIN, LOW);
   //delay(100);
 
-  if(millis () - time_display > 200)
-  {
-    
+  if(millis () - time_display > 200){
     uint32_t speedmm = (156 / (64 /spd))*60;  
     display.clearDisplay();
     //display.drawBitmap(0, 0, home, 128 , 64, SH110X_WHITE);
     display.drawBitmap(0, 0, speed, 128 , 64, SH110X_WHITE);
-    
   
     display.setCursor (48+16, 24);
     display.setTextColor (SH110X_WHITE);
@@ -390,15 +342,13 @@ void loop()
 
     time_display=millis ();
   }
-  
 }
 
 /***************************************************************************************
 ** Function name:           stepMotor
 ** Description:             Step motor "us" microsteps, +ve for forward, -ve for reverse
 ***************************************************************************************/
-void stepMotor(long us)
-{
+void stepMotor(long us){
   if ( us < 0 )
   {
     digitalWrite(MOTOR1_STEP_DIR, LOW);
@@ -416,8 +366,7 @@ void stepMotor(long us)
 ** Function name:           stepMotor2
 ** Description:             Step motor "us" microsteps, +ve for forward, -ve for reverse
 ***************************************************************************************/
-void stepMotor2(long us)
-{
+void stepMotor2(long us){
   if ( us < 0 )
   {
     digitalWrite(MOTOR2_STEP_DIR, LOW);
@@ -435,8 +384,7 @@ void stepMotor2(long us)
 ** Function name:           motorRPM
 ** Description:             Set the motor RPM
 ***************************************************************************************/
-void motorRPM(long rpm)
-{
+void motorRPM(long rpm){
   if ( rpm > MAX_RPM) rpm = MAX_RPM; // Interrup rate limit may lower this again
 
   //Serial.print("Set rpm = "); Serial.print(rpm);
@@ -462,8 +410,7 @@ void motorRPM(long rpm)
 ** Function name:           motorRPM
 ** Description:             Set the motor RPM
 ***************************************************************************************/
-void motor2RPM(long rpm)
-{
+void motor2RPM(long rpm){
   if ( rpm > MAX_RPM) rpm = MAX_RPM; // Interrup rate limit may lower this again
 
   //Serial.print("Set rpm = "); Serial.print(rpm);
@@ -500,8 +447,7 @@ void motor2RPM(long rpm)
   1    1    0    increment timer 1 on T1 (pin 5) falling edge
   1    1    1    increment timer 1 on T1 (pin 5) rising edge
 */
-void startTimer1(void)
-{
+void startTimer1(void){
   // Initialise timer 1
   noInterrupts();                      // disable interrupts
   TCCR1A = 0;                          // stop timer
@@ -538,8 +484,7 @@ void startTimer1(void)
   1    1    0    increment timer 1 on T1 (pin 5) falling edge
   1    1    1    increment timer 1 on T1 (pin 5) rising edge
 */
-void startTimer2(void)
-{
+void startTimer2(void){
   // Initialise timer 2
   noInterrupts();                      // disable interrupts
   TCCR3A = 0;                          // stop timer
@@ -565,8 +510,7 @@ void startTimer2(void)
 ** Function name:           stopTimer1
 ** Description:             Stop the timer interrupt from firing
 ***************************************************************************************/
-void stopTimer1(void)
-{
+void stopTimer1(void){
   noInterrupts();           // disable all interrupts
   TIMSK1 &= ~(1 << TOIE1);  // disable timer overflow interrupt
   interrupts();             // enable all interrupts
@@ -576,8 +520,7 @@ void stopTimer1(void)
 ** Function name:           stopTimer2
 ** Description:             Stop the timer interrupt from firing
 ***************************************************************************************/
-void stopTimer2(void)
-{
+void stopTimer2(void){
   noInterrupts();           // disable all interrupts
   TIMSK3 &= ~(1 << TOIE3);  // disable timer overflow interrupt
   interrupts();             // enable all interrupts
@@ -587,8 +530,7 @@ void stopTimer2(void)
 ** Function name:           TIMER1_OVF_vect
 ** Description:             Called by interrupt when timer 1 overflows
 ***************************************************************************************/
-ISR(TIMER1_OVF_vect)
-{
+ISR(TIMER1_OVF_vect){
   // First load the timer
   TCNT1 = timer1_counter;
 
@@ -609,10 +551,10 @@ ISR(TIMER1_OVF_vect)
 
     // The speed ramp up rate can be set by the controlling funtion
 
-#ifdef AUTO_RAMP_DOWN
-    // Motor will stop in 64 microsteps, so ramp down the speed to reduce jerk
-    if (steps < 64 && timer1_counter > 1000) timer1_counter -= 1000;
-#endif
+  #ifdef AUTO_RAMP_DOWN
+      // Motor will stop in 64 microsteps, so ramp down the speed to reduce jerk
+      if (steps < 64 && timer1_counter > 1000) timer1_counter -= 1000;
+  #endif
   }
 }
 
@@ -620,8 +562,7 @@ ISR(TIMER1_OVF_vect)
 ** Function name:           TIMER3_OVF_vect
 ** Description:             Called by interrupt when timer 1 overflows
 ***************************************************************************************/
-ISR(TIMER3_OVF_vect)
-{
+ISR(TIMER3_OVF_vect){
   // First load the timer
   TCNT3 = timer2_counter;
   digitalWrite(LED_BUILTIN, HIGH);
@@ -645,10 +586,10 @@ ISR(TIMER3_OVF_vect)
     
     // The speed ramp up rate can be set by the controlling funtion
 
-#ifdef AUTO_RAMP_DOWN
+  #ifdef AUTO_RAMP_DOWN
     // Motor will stop in 64 microsteps, so ramp down the speed to reduce jerk
     if (steps < 64 && timer2_counter > 1000) timer2_counter -= 1000;
-#endif
+  #endif
   }
 }
 /***************************************************************************************
